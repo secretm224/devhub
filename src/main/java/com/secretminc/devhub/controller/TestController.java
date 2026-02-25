94ec7a43d935dd838281eba18ebd2b6fcf87c94f
MoonHoSeok <secretm224@gmail.com>
2026-02-25 17:35:41 +0900

    주석 제거

--- src/main/java/com/secretminc/devhub/controller/TestController.java
+++ src/main/java/com/secretminc/devhub/controller/TestController.java
@@ -20,22 +20,6 @@ public class TestController {
         return "DevHub Application is running!";
     }
 
-//    @GetMapping("/db-test")
-//    public String dbTest() {
-//        String url = "jdbc:mysql://dev-secretm-mysql.cerc2oyyyxv5.us-east-1.rds.amazonaws.com:3306/secretm_db";
-//        String username = "admin";
-//        String password = "Tkfkdgo1!";
-//
-//        try {
-//            Class.forName("com.mysql.cj.jdbc.Driver");
-//            java.sql.Connection connection = java.sql.DriverManager.getConnection(url, username, password);
-//            String result = "DB Connection Success! Database: " + connection.getCatalog();
-//            connection.close();
-//            return result;
-//        } catch (Exception e) {
-//            return "DB Connection Failed: " + e.getMessage();
-//        }
-//    }
 
     @GetMapping("/db-test")
     public String dbTest() {
